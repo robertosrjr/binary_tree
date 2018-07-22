ae98d5225fb5621fac3bb45c1b73433c9d3d82cb
Roberto Silva Ramos Junior <robertosrjr@gmail.com>
2018-07-22 16:36:10 -0300

    corrigido

--- ArvoresBinarias/src/ArvoresBinarias.c
+++ ArvoresBinarias/src/ArvoresBinarias.c
@@ -11,68 +11,109 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define size 10
-#define R -1 //Root
-#define E 1 //Left
-#define D 2 //Rigth
-
+//Constantes
+#define tamanho 100
+#define E 0
+#define D 1
+#define R -1
 
 typedef struct str_no {
 
-	char data;
-	int left;
-	int rigth;
-	int father;
+	char dado;
+	int esquerda;
+	int direita;
+	int pai;
 };
 
-struct str_no tree[size];
-int index = 0;
+struct str_no arvore[tamanho];
+int lado,indice=0;
+int opt=-1;
+char pai, no;
 
+void arvore_insere(int pai, char dado, int lado);
+int arvore_procura(char dado);
+void menu_mostrar(void);
 
-int seach_tree(char data) {
+int arvore_procura(char dado){
 
-	if (index != 0) {
+	if (indice != 0) {
 
-		int i;
-		for (i = 0; i < index; i++) {
+		for (int i = 0; i<indice; i++) {
 
-			if (tree[i].data == data) {
+			if (arvore[i].dado == dado) {
 
 				return (i);
 			}
 		}
 	} else {
-
 		return (0);
 	}
 }
 
-void insert_tree(int father, char data, int side) {
+void arvore_insere(int pai, char dado, int lado){
 
-	switch (size) {
+	switch (lado) {
 		case E:
-
-			tree[father].left = index;
-			tree[index].data = data;
-			tree[index].father = father;
-			tree[index].left = -1;
-			tree[index].rigth = -1;
-			index++;
-			break;
+			arvore[pai].esquerda = indice;
+			arvore[indice].dado = dado;
+			arvore[indice].pai = pai;
+			arvore[indice].esquerda = -1;
+			arvore[indice].direita = -1;
+			indice++;
+		break;
 		case D:
-
-			tree[father].rigth = index;
-			tree[index].data = data;
-			tree[index].father = father;
-			tree[index].left = -1;
-			tree[index].rigth = -1;
-			index++;
-			break;
+			arvore[pai].direita = indice;
+			arvore[indice].dado = dado;
+			arvore[indice].pai = pai;
+			arvore[indice].esquerda = -1;
+			arvore[indice].direita = -1;
+			indice++;
+		break;
 	}
 }
 
+void menu_mostrar(void){
 
-int main(void) {
-	puts("Algoritmos e Estrutura de Dados em C - Arvores Binárias"); /* prints Algoritmos e Estrutura de Dados em C - Arvores Binárias */
-	return EXIT_SUCCESS;
+	system("cls");
+	for (int i = 0; i < indice; i++){
+		printf("| %c ",arvore[i].dado);
+	}
+	printf("\n1 - Inserir um NO na arvore");
+	printf("\n2 - Pesquisar um NO na arvore");
+	printf("\n0 - Sair...\n\n");
 }
+
+
+int main(void){
+	int temp;
+	do {
+		menu_mostrar();
+		scanf("%d", &opt);
+		switch (opt){
+			case 1:
+				printf("\nDigite o valor do PAI: ");
+				scanf("%c",&pai);
+				scanf("%c",&pai);
+				printf("Digite o valor do NO: ");
+				scanf("%c",&no);
+				scanf("%c",&no);
+				printf("Digite o lado da subarvore (E=%d/ D=%d/R=%d): ", E,D,R);
+				scanf("%d",&lado);
+				temp = arvore_procura(pai);
+				arvore_insere(temp,no,lado);
+			break;
+			case 2:
+				printf("Digite o valor do NO: ");
+				scanf("%c",&no);
+				scanf("%c",&no);
+				temp = arvore_procura(no);
+				printf("No %c\nFilho Esquerda: %c \nFilho Direita: %c\n\n", arvore[temp].dado, arvore[arvore[temp].esquerda].dado, arvore[arvore[temp].direita].dado);
+				system("pause");
+			break;
+		}
+	} while (opt!=0);
+		system("pause");
+
+	return(0);
+}
+
